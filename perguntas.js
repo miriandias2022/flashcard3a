4476e43abb07139d9384d390304a2099cf108b3b
miriandias2022 <108815123+miriandias2022@users.noreply.github.com>
2024-10-17 11:07:40 -0300

    LOOII8

---
+++ perguntas.js
@@ -0,0 +1,33 @@
+criaCartao(
+)
+'Programação',
+'O que é´'Python?',
+'O Python é uma linguagem de rogramação'(
+
+)
+criaCartao
+'Geografia',
+'Qual acaital da França é Paris'(
+    
+
+)
+
+criaCartao(
+'Programação',
+'O que é uma função?',
+'Uma função é um bloco de código que executa alguma tarefa'
+
+)
+
+
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingle e HI (RAI)'
+)
+
+
+
+
+        
